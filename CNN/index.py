3de82dc691a493d00560c6e70f6f0abde7eae02a
saaeyun <issou0626@naver.com>
2021-05-25 08:05:27 +0900

    CNN

--- CNN/index.py
+++ CNN/index.py
@@ -1,18 +1,45 @@
 from tensorflow.keras import datasets
+from tensorflow.keras import callbacks
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 vocab_size = 10000
 (X_train, y_train), (X_test, y_test) = datasets.imdb.load_data(num_words = vocab_size)
-
+'''
 print(X_train[:5])
-
+'''
 # 패딩
 max_len = 200
 X_train = pad_sequences(X_train, maxlen = max_len)
 X_test = pad_sequences(X_test, maxlen = max_len)
 
 # 패딩되었는지 확인
+'''
 print('X_train의 크기(shape) :', X_train.shape)
 print('X_test의 크기(shape) :', X_test.shape)
 
 print(y_train)
+'''
+
+from tensorflow.keras import models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Dropout, Conv1D, GlobalAveragePooling1D, Dense
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+model = Sequential()
+model.add(Embedding(vocab_size, 256))
+model.add(Dropout(0.3))
+model.add(Conv1D(256, 3, padding = 'valid', activation = 'relu'))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation = 'sigmoid'))
+
+es = EarlyStopping(monitor = 'val_loss', mode = 'min', verbose = 1, patience = 3)
+mc = ModelCheckpoint('best_model.h5', monitor = 'val_acc', mode = 'max', verbose = 1, save_best_only = True)
+
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['acc'])
+history = model.fit(X_train, y_train, epochs = 20, validation_data = (X_test, y_test), callbacks = [es, mc])
+
+loaded_model = load_model('best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
